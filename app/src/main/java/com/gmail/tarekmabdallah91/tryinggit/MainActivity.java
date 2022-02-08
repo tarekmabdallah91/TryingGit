b0b657af53659f6c6812f0f28fb71d8f150d651e
tarekmabdallah91 <tarekmabdallah91@gmail.com>
2022-02-08 13:33:48 +0300

    2nd commit in  hotfix-develop

--- app/src/main/java/com/gmail/tarekmabdallah91/tryinggit/MainActivity.java
+++ app/src/main/java/com/gmail/tarekmabdallah91/tryinggit/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // THIS IS hotfix-develop
+        // 2nd commit
     }
 }
