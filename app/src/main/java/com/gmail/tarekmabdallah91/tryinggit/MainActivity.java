6797936fcabda50dea655e6b100a2815223b51d9
tarekmabdallah91 <tarekmabdallah91@gmail.com>
2022-02-08 13:16:59 +0300

    Initial commit

---
+++ app/src/main/java/com/gmail/tarekmabdallah91/tryinggit/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gmail.tarekmabdallah91.tryinggit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // THIS IS master
+    }
+}
